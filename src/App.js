0aae2ab93ce32afb6e24e2baaed27968569e5b68
nburbinski <nburbinski@gmail.com>
2020-05-04 16:54:58 -0400

    fetch data from general api endpoint, send to table component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 
 // Import components
 import Table from "./components/Table";
 
 const App = () => {
+  const [data, setData] = useState({});
+  console.log(data);
+
+  useEffect(() => {
+    fetch(`https://data.cdc.gov/resource/hc4f-j6nb.json?`).then((res) =>
+      res.json().then((data) => setData(data))
+    );
+  }, []);
   return (
     <>
       <h1>COVID</h1>
-      <Table />
+      <Table data={data} />
     </>
   );
 };
