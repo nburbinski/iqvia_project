643575c9c6e1c89b7679ae405d6f0f4ad008faa5
nburbinski <nburbinski@gmail.com>
2020-05-04 20:48:56 -0400

    changed date format and default 0

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 const TableRow = ({ data, total }) => {
+  const startDate = new Date(data.start_week);
+  const endDate = new Date(data.end_week);
+
   return (
     <tr>
       <td>{data.state}</td>
-      <td>{new Date(data.start_week).toDateString()}</td>
-      <td>{new Date(data.end_week).toDateString()}</td>
-      <td>{data.covid_deaths}</td>
       <td>
-        {data.covid_deaths ? (data.covid_deaths / total).toFixed(2) * 100 : 0}%
+        {`${
+          startDate.getMonth() + 1
+        }/${startDate.getDate()}/${startDate.getFullYear()}`}
+      </td>
+      <td>
+        {`${
+          endDate.getMonth() + 1
+        }/${endDate.getDate()}/${endDate.getFullYear()}`}
+      </td>
+      <td>{data.covid_deaths ? data.covid_deaths : 0}</td>
+      <td>
+        {data.covid_deaths ? ((data.covid_deaths / total) * 100).toFixed(2) : 0}
+        %
       </td>
     </tr>
   );
